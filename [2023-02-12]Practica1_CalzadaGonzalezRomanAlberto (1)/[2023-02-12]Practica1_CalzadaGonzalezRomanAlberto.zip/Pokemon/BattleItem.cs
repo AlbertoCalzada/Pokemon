e65ca60c14d860537668d4837426ffa6236a2c5a
AlbertoCalzada <113586207+AlbertoCalzada@users.noreply.github.com>
2023-03-22 10:06:17 +0100

    Add files via upload

---
+++ [2023-02-12]Practica1_CalzadaGonzalezRomanAlberto (1)/[2023-02-12]Practica1_CalzadaGonzalezRomanAlberto.zip/Pokemon/BattleItem.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pokemon
+{
+    abstract class BattleItem:Item
+    {
+        public BattleItem(string name, int buyPricePokedollars, int buyPricePokemillas, int buyPriceBattlePoints, int sellPricePokedollars, int sellPricePokemillas, int sellPriceBattlePoints)
+            : base(name, buyPricePokedollars, buyPricePokemillas, buyPriceBattlePoints, sellPricePokedollars, sellPricePokemillas, sellPriceBattlePoints)
+        {
+
+        }
+        public BattleItem() : base() { }
+        public override bool UseItemOutCombat()
+        {
+            return true;
+        }
+        public override bool ThrowItem()
+        {
+            return true;
+        }
+
+        public override bool UseItemInCombat()
+        {
+            return true;
+        }
+
+        public override void Utility(IndividualPokemon pokemon)
+        {
+
+        }
+        public override bool Buy()
+        {
+            return true;
+        }
+
+        public override bool Sell()
+        {
+            return true;
+        }
+    }
+    class Pokedoll : BattleItem
+    {
+        public Pokedoll(string name, int buyPricePokedollars, int buyPricePokemillas, int buyPriceBattlePoints, int sellPricePokedollars, int sellPricePokemillas, int sellPriceBattlePoints)
+        : base(name, buyPricePokedollars, buyPricePokemillas, buyPriceBattlePoints, sellPricePokedollars, sellPricePokemillas, sellPriceBattlePoints) 
+        { 
+        
+        
+        }
+        public Pokedoll() : base() { }
+        //A continuaciñon tenemos las instancias de los objetos pertenicientes a esta clase.
+        public Pokedoll AssignPokedoll()
+        {
+            Pokedoll P1 = new Pokedoll("Pokedoll", 100, 100, 100, 100, 100, 100);
+            P1.AddQuantity(1);
+            return P1;
+        }
+        public override void Utility(IndividualPokemon pokemon)
+        {
+            pokemon.SetHasEscaped(true);
+            
+        }
+    }
+}
